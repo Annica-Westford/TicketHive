b9e6564977c9cf7fc2d166381f114a5db3beeda4
Annica Westford <annica.westford@gmail.com>
2023-04-05 11:47:41 +0200

    FixedPopUp

--- TicketHive/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TicketHive/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -8,16 +8,20 @@ using TicketHive.Server.Repos.UsersRepo;
 
 namespace TicketHive.Server.Areas.Identity.Pages.Account
 {
-    [BindProperties]
+    
     public class LoginModel : PageModel
     {
         private readonly IUsersRepo repo;
 
+        [BindProperty]
         [Required(ErrorMessage = "Username is required")]
+
         public string? Username { get; set; }
 
+        [BindProperty]
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
+        public string Message { get; set; } = string.Empty;
 
         public LoginModel(IUsersRepo repo)
         {
@@ -29,6 +33,7 @@ namespace TicketHive.Server.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost()
         {
+
             if (ModelState.IsValid)
             {
                 var signInResult = await repo.SignInUserWithUserName(Username!, Password!, false, false);
@@ -38,8 +43,12 @@ namespace TicketHive.Server.Areas.Identity.Pages.Account
                     //lägg in länk till vart man ska skickas när man loggat in!
                     return Redirect("~/home");
                 }
+                else
+                {
+                    Message = "Failed to log in";
+                }
             }
-
+            
             return Page();
         }
     }
