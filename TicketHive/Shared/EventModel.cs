0bb9a9536b0604d4df59294b4f6609d6c4407602
Annica Westford <annica.westford@gmail.com>
2023-04-11 16:06:58 +0200

    La till bilder bl a

--- TicketHive/Shared/EventModel.cs
+++ TicketHive/Shared/EventModel.cs
@@ -1,17 +1,33 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace TicketHive.Shared
 {
     public class EventModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Event Type is required")]
         public string EventType { get; set; } = null!;
+
+        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; } = null!;
         public string? Image { get; set; }
+
+        [Required(ErrorMessage = "Date and time is required")]
+        [Compare(nameof(DateTime.Now), ErrorMessage = "Date and time must be after today's date")]
         public DateTime DateTime { get; set; }
+
+        [Required(ErrorMessage = "TicketPrice is required")]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ErrorMessage = "TicketPrice must be a positive number")]
         public decimal TicketPrice { get; set; }
+
+        [Required(ErrorMessage = "Max attendees is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "MaxCapacity must be a positive number")]
         public int MaxCapacity { get; set; }
         public int SoldTickets { get; set; }
         public bool IsFullyBooked { get; set; }
