35461a45e11af8e3041ae281e75acb3e3d93b305
Annica Westford <annica.westford@gmail.com>
2023-04-13 21:21:39 +0200

    Fixed some small stuff

--- TicketHive/Client/Api/ApiHelper.cs
+++ TicketHive/Client/Api/ApiHelper.cs
@@ -5,21 +5,7 @@ namespace TicketHive.Client.Api
 {
     public class ApiHelper
     {
-        //Har kört InitializeClient i Program.cs
-        //Har lagt till ApiAccessKey i appsettings.json - ska kunna nås via Iconfiguration
-        //Har lagt till en ApiModel - OBS tänk på att Result kommer tillbaka som DOUBLE
-        //Enligt Daniel: Det viktiga är att api key sätts in i headern på ett request, inte i query eller url
-
-        //private readonly IConfiguration config;
-        //private readonly string apiKey;
-
         public static HttpClient HttpClient { get; set; } = new();
-
-        public ApiHelper(/*IConfiguration config*/)
-        {
-            //this.config = config;
-            //apiKey = config.GetValue<string>("ApiAccessKey");
-        }
 
         public static void InitializeClient()
         {
---
+++ TicketHive/Client/Services/CurrencyService.cs
@@ -0,0 +1,32 @@
+﻿using TicketHive.Client.Api;
+using TicketHive.Shared.Api;
+
+namespace TicketHive.Client.Services
+{
+    public class CurrencyService
+    {
+        public async Task<decimal?> ConvertAmount(string country, decimal price)
+        {
+            //Get exchange rates from ApiHelper
+            Root? root = await new ApiHelper().GetExchangeRates();
+
+            if (root != null)
+            {
+                if (country == "Sweden" || string.IsNullOrEmpty(country))
+                {
+                    return price;
+                }
+                else if (country == "Great_Britain")
+                {
+                    return price * Convert.ToDecimal(root.Rates.GBP);
+                }
+                else
+                {
+                    return price * Convert.ToDecimal(root.Rates.EUR);
+                }
+            }
+
+            return null;
+        }
+    }
+}
