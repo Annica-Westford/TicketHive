ded07b776d50e907295a756f7439dd231de942e1
Annica Westford <annica.westford@gmail.com>
2023-04-15 18:22:19 +0200

    Fixed currency logic

--- TicketHive/Client/Services/CurrencyService.cs
+++ TicketHive/Client/Services/CurrencyService.cs
@@ -5,53 +5,58 @@ namespace TicketHive.Client.Services
 {
     public class CurrencyService : ICurrencyService
     {
+        private decimal? exchangeRateGBP;
+        private decimal? exchangeRateEUR;
+        public async Task SetExchangeRates()
+        {
+            //Get exchange rates from ApiHelper
+            Root? exchangeRates = await ApiHelper.GetExchangeRates();
+
+            if (exchangeRates != null)
+            {
+                exchangeRateGBP = Convert.ToDecimal(exchangeRates.Rates.GBP);
+                exchangeRateEUR = Convert.ToDecimal(exchangeRates.Rates.EUR);
+            }
+        }
         public async Task<decimal?> GetExchangeRate(string country)
         {
-            //Get exchange rates from ApiHelper
-            Root? exchangeRates = await ApiHelper.GetExchangeRates();
-
-            if (exchangeRates != null)
+            if (country == "Sweden" || string.IsNullOrEmpty(country))
             {
-                if (country == "Sweden" || string.IsNullOrEmpty(country))
-                {
-                    return 1;
-                }
-                if (country == "Great_Britain")
-                {
-                    return Convert.ToDecimal(exchangeRates.Rates.GBP);
-                }
-                else
-                {
-                    return Convert.ToDecimal(exchangeRates.Rates.EUR);
-                }
+                return 1;
             }
-
-            return null;
+            if (country == "Great_Britain")
+            {
+                return exchangeRateGBP;
+            }
+            else
+            {
+                return exchangeRateEUR;
+            }
         }
 
-        public async Task<decimal?> ConvertAmount(string country, decimal price)
-        {
-            //Get exchange rates from ApiHelper
-            Root? exchangeRates = await ApiHelper.GetExchangeRates();
+        //public async Task<decimal?> ConvertAmount(string country, decimal price)
+        //{
+        //    //Get exchange rates from ApiHelper
+        //    Root? exchangeRates = await ApiHelper.GetExchangeRates();
 
-            if (exchangeRates != null)
-            {
-                if (country == "Sweden" || string.IsNullOrEmpty(country))
-                {
-                    return price;
-                }
-                else if (country == "Great_Britain")
-                {
-                    return price * Convert.ToDecimal(exchangeRates.Rates.GBP);
-                }
-                else
-                {
-                    return price * Convert.ToDecimal(exchangeRates.Rates.EUR);
-                }
-            }
+        //    if (exchangeRates != null)
+        //    {
+        //        if (country == "Sweden" || string.IsNullOrEmpty(country))
+        //        {
+        //            return price;
+        //        }
+        //        else if (country == "Great_Britain")
+        //        {
+        //            return price * Convert.ToDecimal(exchangeRates.Rates.GBP);
+        //        }
+        //        else
+        //        {
+        //            return price * Convert.ToDecimal(exchangeRates.Rates.EUR);
+        //        }
+        //    }
 
-            return null;
-        }
+        //    return null;
+        //}
 
         public string GetCurrencyTitle(string country)
         {
